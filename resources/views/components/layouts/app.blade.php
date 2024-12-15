ae106e9d74eef88b07e970e708a77fbec0d0e337
FihramCahyo <fihramcahyo10@gmail.com>
2024-12-15 12:10:29 +0700

    Update preline & navbar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+
+        @livewireStyles
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     </head>
     <body>
         @livewire('layouts.menu-navigation')
         {{ $slot }}
+
+        @livewireScripts
     </body>
 </html>
